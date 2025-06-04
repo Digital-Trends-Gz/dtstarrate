ef1d3aa259da93f7cffaf7266b764836f986f57a
ziadaboalkamar <ziad-lovelyboy@hotmail.com>
2025-06-04 16:42:16 +0300

    update plugin translate

--- dt-star-rating.php
+++ dt-star-rating.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: DT Star Rating System
  * Description: Adds a star rating to posts with IP and cookie-based voting protection.
- * Version: 1.10
+ * Version: 1.11
  * Author: D.T. Company
  */
 
