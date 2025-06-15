0e445fd2c0cad6305a2f729d230c2c034389b539
ziadaboalkamar <ziad-lovelyboy@hotmail.com>
2025-06-15 16:41:58 +0300

    update the star

--- dt-star-rating.php
+++ dt-star-rating.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: DT Star Rating System
  * Description: Adds a star rating to posts with IP and cookie-based voting protection.
- * Version: 1.29
+ * Version: 1.30
  * Author: D.T. Company
  */
 
@@ -197,7 +197,7 @@ add_shortcode('star_rating', function () {
 
                 <div class="rated-reviews">
                     <span class="rated-reviews__counter2 " id="js-ratingValue2"><?php echo $already_rated ? 'Thank you!' : 'Rate Us'; ?></span>
-                    <div id="star-rating " class="<?php echo $already_rated ?'star_rated' : "" ?>" data-postid="<?php echo $post_id; ?>" data-rated="<?php echo $already_rated ? '1' : '0'; ?>">
+                    <div id="star-rating" class="<?php echo $already_rated ?'star_rated' : "" ?>" data-postid="<?php echo $post_id; ?>" data-rated="<?php echo $already_rated ? '1' : '0'; ?>">
                         <?php for ($i = 1; $i <= 5; $i++): ?>
                             <span class="star" data-value="<?php echo $i; ?>">&#9733;</span>
                         <?php endfor; ?>
@@ -230,10 +230,10 @@ add_shortcode('star_rating', function () {
                 "<?php echo esc_js($app_name); ?> APP",
                 "<?php echo esc_js($app_name); ?>.com"
               ],
-              "url": "<?php echo esc_url(get_permalink()); ?>",
-              <?php if (!empty($current_language)): ?>
-              "inLanguage": "<?php echo esc_js($current_language); ?>",
-              <?php endif; ?>
+            "url": "<?php echo esc_url( !empty($current_language) ? home_url("/$current_language/") : home_url('/') ); ?>",
+            <?php if (!empty($current_language)) : ?>
+            "inLanguage": "<?php echo esc_js($current_language); ?>",
+            <?php endif; ?>                   
               "image": "<?php echo esc_url($logo_url); ?>",
               "operatingSystem": "Windows, Linux, iOS, Android, OSX, macOS",
               "applicationCategory": "UtilitiesApplication",
